b1591eb2928338d56941eb3f6811300fe3119476
devmirza <mamasaidovmirzabek@gmail.com>
2022-04-04 16:31:11 +0500

    verify email

---
+++ src/main/java/uz/pdp/appinstagram/component/MailSender.java
@@ -0,0 +1,30 @@
+package uz.pdp.appinstagram.component;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailSender {
+
+    @Bean
+    public JavaMailSender getEmail() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setPort(587);
+        javaMailSender.setProtocol("smtp");
+        javaMailSender.setUsername("bootcampzoom97@gmail.com");
+        javaMailSender.setPassword("#bootcamp");
+
+
+        Properties properties = javaMailSender.getJavaMailProperties();
+        properties.put("mail.transport.protocol", "true");
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.debug", "true");
+        return javaMailSender;
+    }
+}
