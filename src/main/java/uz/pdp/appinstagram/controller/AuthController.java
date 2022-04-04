9886bbc2a02de465f698d31602a8d6f88b1c5902
muxriddinjalilovfaxriddinovich <muxriddinjalilov14@gmail.com>
2022-04-04 13:28:51 +0500

    jwt

---
+++ src/main/java/uz/pdp/appinstagram/controller/AuthController.java
@@ -0,0 +1,35 @@
+package uz.pdp.appinstagram.controller;
+
+//import com.example.soliqjwttask.dto.LoginDTO;
+//import com.example.soliqjwttask.security.JwtProvider;
+//import com.example.soliqjwttask.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.bind.annotation.*;
+import uz.pdp.appinstagram.payload.LoginDTO;
+import uz.pdp.appinstagram.security.JwtProvider;
+import uz.pdp.appinstagram.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    AuthService authService;
+
+    @Autowired
+    JwtProvider jwtProvider;
+
+    @PostMapping("/login")
+    public HttpEntity<?> login(@RequestBody LoginDTO loginDTO){
+        String token=jwtProvider.generateToken(loginDTO.getUserName());
+        return ResponseEntity.ok().body(token);
+    }
+
+
+}
